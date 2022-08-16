cb553d9866c6698baefb7c40c8c1345437d40e86
fsoto <fsoto@imail.ag>
2022-08-16 15:58:26 -0600

    escape $ commands

--- zsh_update.sh
+++ zsh_update.sh
@@ -2,8 +2,8 @@ python3 -m pip install --upgrade pip
 pip3 install ansible
 
 tee -a ~/.zshrc << EOF
-export PATH=$HOME/bin:/usr/local/bin:$PATH
-export PATH=$HOME/Library/Python/3.8/bin:$PATH
+export PATH=\$HOME/bin:/usr/local/bin:\$PATH
+export PATH=\$HOME/Library/Python/3.8/bin:\$PATH
 EOF
 
 source ~/.zshrc
