2f604b27a3579343af3a7d1fdb6dcc0ff9a2d693
fsoto <fsoto@imail.ag>
2022-08-16 15:54:17 -0600

    .sh file to configure ansible

---
+++ zsh_update.sh
@@ -0,0 +1,9 @@
+python3 -m pip install --upgrade pip
+pip3 install ansible
+
+tee -a ~/.zshrc << EOF
+export PATH=$HOME/bin:/usr/local/bin:$PATH
+export PATH=$HOME/Library/Python/3.8/bin:$PATH
+EOF
+
+source ~/.zshrc
